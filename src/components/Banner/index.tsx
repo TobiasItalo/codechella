407db783b2c3fb24c6b456bf976fd08767c415e3
Italo Tobias <tobiasitalo@gmail.com>
2023-06-10 17:09:00 -0300

    :lipstick: feat: tweak css

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,15 +3,17 @@ import './Banner.css'
 
 interface BannerProps {
   page: string
-  text: string
+  firstLine: string
+  secondLine?: string
 }
 
-export default function Banner({ page, text }: BannerProps): JSX.Element {
+export default function Banner({ page, firstLine, secondLine }: BannerProps): JSX.Element {
   const { theme, device } = useSettingsContext()
 
   return (
-    <div className="banner-container" style={{ backgroundImage: `url("src/assets/${theme}/images/${device}/banner-${page}.png")` }}>
-      <p className={`banner-container__text banner-container__text-${theme}`}>{text}</p>
-    </div>
+    <section className="banner-container" style={{ backgroundImage: `url("src/assets/${theme}/images/${device}/banner-${page}.png")` }}>
+      <p className={`banner-container__text banner-container__text-${theme}`}>{firstLine}</p>
+      <p className={`banner-container__text banner-container__text-${theme}`}>{secondLine}</p>
+    </section>
   )
 }
